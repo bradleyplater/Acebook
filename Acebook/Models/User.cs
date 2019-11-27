3378cc5313297b0c524bb1961e3f963a93215032
bradleyplater <bradley.plater1@gmail.com>
2019-11-27 14:06:48 +0000

    Created User class with basic properties

---
+++ Acebook/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Acebook.Models
+{
+    public class User
+    {
+
+        public User(int id, string name, string email, string password, Array posts)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            Posts = posts;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+        public string Email { get; }
+        public string Password { get; }
+        public Array Posts { get; }
+
+    }
+}
