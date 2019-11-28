0b5e6c5a6828a84f9d19973aecce09e1f1567736
bradleyplater <bradley.plater1@gmail.com>
2019-11-28 13:41:28 +0000

    Changed file structure to use ASP.net Core, Instead of ASP.net Framework

--- Acebook/Models/User.cs
+++ Acebook/Models/User.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 
 namespace Acebook.Models
 {
     public class User
     {
-
         public User(int id, string name, string email, string password, Array posts)
         {
             Id = id;
@@ -15,9 +14,12 @@ namespace Acebook.Models
             Email = email;
             Password = password;
             Posts = posts;
-        }
 
+
+        }
         public int Id { get; }
+
+
         public string Name { get; }
         public string Email { get; }
         public string Password { get; }
