d92335a73982d50572c3213cedfe66b6e6cfbe8d
bradleyplater <bradley.plater1@gmail.com>
2019-11-29 12:50:41 +0000

    Re-Added tests

--- AcebookTests/UnitTest1.cs
+++ AcebookTests/UnitTest1.cs
@@ -1,17 +1,38 @@
+using Acebook.Models;
 using NUnit.Framework;
 
 namespace AcebookTests
 {
     public class Tests
     {
+        User user;
+
+        int id;
+        string name;
+        string email;
+        string password;
+        Post[] posts = new Post[] { new Post(), new Post()};
+
         [SetUp]
         public void Setup()
         {
+            id = 2;
+            name = "John";
+            email = "John@gmail.com";
+            password = "Password";
+
+            user = new User(id, name, email, password, posts);
         }
 
         [Test]
-        public void Test1()
+        public void User_Initialize_Properties()
         {
+            Assert.AreEqual(user.Id, id);
+            Assert.AreEqual(user.Name, name);
+            Assert.AreEqual(user.Email, email);
+            Assert.AreEqual(user.Password, password);
+            Assert.AreEqual(user.Posts, posts);
+
         }
     }
 }
