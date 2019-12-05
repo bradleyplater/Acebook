8afc028a6449fc80205ec3298fa392536df2b7a3
bradleyplater <bradley.plater1@gmail.com>
2019-12-05 14:09:22 +0000

    Added comments to the DBhelper

--- Acebook/Models/DBhelper.cs
+++ Acebook/Models/DBhelper.cs
@@ -9,7 +9,7 @@ namespace Acebook.Models
 {
     public class DBhelper
     {
-
+		//Connects to the mongo database and retrieves the collection which is specified by the function calling the code
         public static IMongoCollection<BsonDocument> ConnectToDB(string dbName, string collectionName)
         {
 			var connectionString = "mongodb+srv://Makers1:Admin@acebook-ye6db.mongodb.net/test?retryWrites=true&w=majority";
@@ -18,14 +18,9 @@ namespace Acebook.Models
 			Console.WriteLine("I got here 1");
 			return database.GetCollection<BsonDocument>(collectionName);
         }
-		public static IMongoCollection<Post> ConnectToDBForPost(string dbName, string collectionName)
-		{
-			var connectionString = "mongodb+srv://Makers1:Admin@acebook-ye6db.mongodb.net/test?retryWrites=true&w=majority";
-			var client = new MongoClient(connectionString);
-			var database = client.GetDatabase(dbName);
-			return database.GetCollection<Post>(collectionName);
-		}
 
+		/* Takes parameters and checks if a user exists from checking the database against those parameters
+		 * returns a filled user object if a user exists and returns an empty one if not */
 		public static User CheckIfUserExists(string email, string password)
 		{
 
@@ -44,40 +39,20 @@ namespace Acebook.Models
 					string surname = (string)document.GetValue("Surname");
 					string username = (string)document.GetValue("Username");
 
-					return new User(
-						id,
-						firstname,
-						surname,
-						username,
-						email,
-						password
-						); 
+					return new User(id,firstname,surname,username,email,password); 
 				}
 				else
 				{
-					return new User(
-						new BsonObjectId(new ObjectId()),
-						"",
-						"",
-						"",
-						"",
-						""
-						);
+					return new User(new BsonObjectId(new ObjectId()),"","","","","");
 				}
 			}
 			else
 			{
-				return new User(
-						new BsonObjectId(new ObjectId()),
-						"",
-						"",
-						"",
-						"",
-						""
-						);
+				return new User(new BsonObjectId(new ObjectId()),"","","","","");
 			}
 		}
 
+		//Creates a new user document within the mongo database
 		public static void CreateNewUser(string Firstname, string Surname, string Email, string Username, string Password)
 		{
 			var collection = ConnectToDB("Acebook", "User");
@@ -95,6 +70,7 @@ namespace Acebook.Models
 			collection.InsertOne(document);
 		}
 
+		//Checks if the username provided exists in the database
 		public static bool CheckUsernameExists(string Username)
 		{
 			var collection = ConnectToDB("Acebook", "User");
@@ -110,6 +86,8 @@ namespace Acebook.Models
 				return false;
 			}
 		}
+
+		//Checks if the email provided exists in the database
 		public static bool CheckEmailExists(string Email)
 		{
 			var collection = ConnectToDB("Acebook", "User");
@@ -126,6 +104,7 @@ namespace Acebook.Models
 			}
 		}
 
+		//Creates a post document within the mongo database
 		public static void CreatePost(string Firstname, string Surname, string Username, string Body, DateTime Date)
 		{
 			var collection = ConnectToDB("Acebook", "Posts");
@@ -145,6 +124,7 @@ namespace Acebook.Models
 			collection.InsertOne(document);
 		}
 
+		//Returns a list of all posts that are stored within the mongo database
 		public static List<BsonDocument> GetAllPosts()
 		{
 
@@ -156,6 +136,7 @@ namespace Acebook.Models
 	
 		}
 
+		//Adds a like to an existing post document
 		public static void AddLike(BsonDocument document, string id)
 		{
 			bool userLike = false;
@@ -201,6 +182,7 @@ namespace Acebook.Models
 			
 		}
 
+		//Adds a dislike to an existing post document
 		public static void AddDislike(BsonDocument document, string id)
 		{
 			bool userDislike = false;
@@ -246,6 +228,7 @@ namespace Acebook.Models
 			}
 		}
 
+		//Removes a like from a post document
 		public static void RemoveLike(BsonArray like, IMongoCollection<BsonDocument> collection, int index, FilterDefinition<BsonDocument> filter)
 		{
 			like.RemoveAt(index);
@@ -253,6 +236,7 @@ namespace Acebook.Models
 			collection.UpdateOne(filter, updateLike);
 		}
 
+		//Removes a dislike from a post document
 		public static void RemoveDislike(BsonArray dislike, IMongoCollection<BsonDocument> collection, int index, FilterDefinition<BsonDocument> filter)
 		{
 			dislike.RemoveAt(index);
@@ -260,6 +244,7 @@ namespace Acebook.Models
 			collection.UpdateOne(filter, updateDislike);
 		}
 
+		//Searches through the likes on a post and when the id for current user matches the id within a like it breaks and returns the id and the index
 		public static Tuple<BsonValue, int> SearchThroughLike(BsonArray like, string id)
 		{
 			var index = -1;
@@ -276,6 +261,7 @@ namespace Acebook.Models
 			return Tuple.Create(x, index);
 		}
 
+		//Searches through the dislikes on a post and when the id for current user matches the id within a dislike it breaks and returns the id and the index
 		public static Tuple<BsonValue, int> SearchThroughDislike(BsonArray dislike, string id)
 		{
 			var index = -1;
@@ -292,6 +278,7 @@ namespace Acebook.Models
 			return Tuple.Create(x, index);
 		}
 
+		//Returns a singular document by position in the array
 		public static BsonDocument SearchForDocument(int count)
 		{
 			var documents = GetAllPosts();
